86a38cf086e02d3cad4bc07d8a28a6a35e5577b3
rezavradana <rezapradana472@gmail.comecho>
2024-05-17 20:28:02 +0700

    Mengimplementasikan UI/UX ke landing page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,9 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
 
-console.log('Hello Coders! :)');
 
 const app = new App({
     button: document.querySelector('#hamburgerButton'),
     drawer: document.querySelector('#navigationDrawer'),
     content: document.querySelector('#mainContent'),
-  });
+});
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,14 +1,14 @@
 import DrawerInitiator from '../utils/drawer-initiator';
- 
+
 class App {
   constructor({ button, drawer, content }) {
     this._button = button;
     this._drawer = drawer;
     this._content = content;
- 
+
     this._initialAppShell();
   }
- 
+
   _initialAppShell() {
     DrawerInitiator.init({
       button: this._button,
@@ -16,6 +16,19 @@ class App {
       content: this._content,
     });
   }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+    const mainContent = document.querySelector('#mainContent');
+    skipLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      mainContent.scrollIntoView({ behavior: 'smooth' });
+      skipLink.blur();
+    });
+  }
 }
- 
+
 export default App;
