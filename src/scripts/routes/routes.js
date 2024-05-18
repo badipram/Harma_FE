9afd076476801fd68022ef428580f7ae73d1de9f
rezavradana <rezapradana472@gmail.comecho>
2024-05-18 22:25:36 +0700

    Committing my changes before merge

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,13 +2,17 @@ import AboutUs from '../views/pages/about-us';
 import Beranda from '../views/pages/beranda';
 import Fasilitas from '../views/pages/fasilitas';
 import Kegiatan from '../views/pages/kegiatan';
+import FormFasilitas from '../views/pages/form-fasilitas';
+import FormKegiatan from '../views/pages/form-kegiatan';
 
 const routes = {
-    '/': Beranda,
-    '/beranda': Beranda,
-    '/fasilitas': Fasilitas,
-    '/kegiatan' : Kegiatan,
-    '/about-us': AboutUs,
+  '/': Beranda,
+  '/beranda': Beranda,
+  '/fasilitas': Fasilitas,
+  '/kegiatan': Kegiatan,
+  '/about-us': AboutUs,
+  '/fasilitas/tambah': FormFasilitas,
+  '/kegiatan/tambah': FormKegiatan,
 };
 
 export default routes;
