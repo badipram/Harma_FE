06cc52b1b1a4a91b8d4f06f6884fa17a70112f55
badi amnu <badipram23@outlook.com>
2024-05-18 16:56:59 +0700

    menambahkan halaman login dan about us

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,6 +2,7 @@ import AboutUs from '../views/pages/about-us';
 import Beranda from '../views/pages/beranda';
 import Fasilitas from '../views/pages/fasilitas';
 import Kegiatan from '../views/pages/kegiatan';
+import Login from '../views/pages/login';
 
 const routes = {
     '/': Beranda,
@@ -9,6 +10,7 @@ const routes = {
     '/fasilitas': Fasilitas,
     '/kegiatan' : Kegiatan,
     '/about-us': AboutUs,
+    '/login': Login,
 };
 
 export default routes;
