1441444621b572692b35d290828f92d997043969
badi amnu <badipram23@outlook.com>
2024-05-20 07:36:04 +0700

    merapihkan halaman login

--- src/scripts/views/templates/template-login-helper.js
+++ src/scripts/views/templates/template-login-helper.js
@@ -2,18 +2,24 @@ const createLoginTemplate = (login) =>`
         <div class="login-box">
                 <h2>Login</h2>
                 <form>
-                    <label for="email">E-mail address</label>
-                    <input type="email" id="email" name="email" placeholder="E-mail address" required>
-                    
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Password" required>
-                    
-                    <div class="remember-me-container">
-                    <input type="checkbox" id="remember-me" name="remember-me">
-                    <label for="remember-me">Ingat Saya</label>
+                    <div class="form-input">
+                        <label for="email">E-mail address</label>
+                        <input type="email" id="email" name="email" placeholder="E-mail address" required>
                     </div>
 
-                    <button type="submit">Login</button>
+                    <div class="form-input">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" required>
+                    </div>
+
+                    <div class="remember-me-container">
+                        <input type="checkbox" id="remember-me" name="remember-me">
+                        <label for="remember-me">Ingat Saya</label>
+                    </div>
+
+                    <div class="form-button">
+                        <button type="submit">Login</button>
+                    </div> 
                 </form>
             </div>
 `;
