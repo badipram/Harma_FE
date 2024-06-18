6a6ecd2ba19c0390201581e4739996adc3390e88
badi amnu <badipram23@outlook.com>
2024-06-18 18:44:00 +0700

    fixing webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,23 @@
+// const { merge } = require('webpack-merge');
+// const path = require('path');
+// const common = require('./webpack.common');
+
+// module.exports = merge(common, {
+//   mode: 'development',
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     static: path.resolve(__dirname, 'dist'),
+//     open: true,
+//     compress: true,
+//     client: {
+//       overlay: {
+//         errors: true,
+//         warnings: false,
+//       },
+//     },
+//   },
+// });
+
 const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
