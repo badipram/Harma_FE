5ad80ea53ebeccfa3dbaa4f8262280fd42a73bd5
badi amnu <badipram23@outlook.com>
2024-06-18 14:56:17 +0700

    delete backend from frontend

---
+++ src/public/team-member.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+const teamMembers = [
+  {
+    name: 'TB Muhammad Iqbal Zikri',
+    university: 'Universitas Cendekia Abditama',
+    role: 'Back-End Developer',
+    imgSrc: 'path/to/photo1.jpg',
+    alt: 'TB Muhammad Iqbal Zikri',
+  },
+  {
+    name: 'Reza Pradana',
+    university: 'Universitas Pamulang',
+    role: 'Front-End Developer',
+    imgSrc: './images/reza.jpg',
+    alt: 'Reza Pradana',
+  },
+  {
+    name: 'Badi Amnu Pramuditnya',
+    university: 'Universitas Pamulang',
+    role: 'UI/UX Designer',
+    imgSrc: './images/badi.jpg',
+    alt: 'Badi Amnu Pramuditnya',
+  },
+];
+
+export { teamMembers };
--- src/scripts/views/templates/template-about-us-helper.js
+++ src/scripts/views/templates/template-about-us-helper.js
@@ -1,41 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-unused-vars */
+import { teamMembers } from '../../../public/team-member';
+
 const createAboutUsTemplate = () => `
     <div class="about-header" data-aos="fade-down" data-aos-duration="1200">
-            <img src="./images/brand-logo.png" alt="HARMA Logo" class="about-logo">
-            <div class="about-description">
-            <h3>Apakah itu HARMA?</h3>
+        <img src="./images/brand-logo.png" alt="HARMA Logo" class="about-logo" loading="lazy">
+        <div class="about-description">
+            <h3>Apa itu HARMA?</h3>
             <p>Platform digital yang dapat memberikan akses informasi dan memantau kegiatan apa saja yang sedang terjadi di lingkungan masyarakat khususnya cakupan RT/RW. Platform ini bertujuan untuk meningkatkan keterlibatan antar masyarakat dalam berinteraksi sosial.</p>
         </div>
     </div>
 
-    <div class="team-section" data-aos="fade-up" data-aos-duration="1200">
-        <div class="team-member">
-            <img src="path/to/photo1.jpg" alt="TB Muhammad Iqbal Zikri" class="team-photo">
-        <div class="team-info">
-            <h3>TB Muhammad Iqbal Zikri</h3>
-            <p>Universitas Cendekia Abditama</p>
-            <p>Back-End Developer</p>
-        </div>
+    <div class="team-section">
+        ${teamMembers.map((member, index) => `
+            <div class="team-member" data-aos="fade-up" data-aos-duration="1200">
+                <img src="${member.imgSrc}" alt="${member.alt}" class="team-photo" loading="lazy">
+                <div class="team-info">
+                    <h3>${member.name}</h3>
+                    <p>${member.university}</p>
+                    <p>${member.role}</p>
+                </div>
+            </div>
+        `).join('')}
     </div>
+`;
 
-        <div class="team-member" data-aos="fade-up" data-aos-duration="1200">
-            <img src="path/to/photo2.jpg" alt="Reza Pradana" class="team-photo">
-        <div class="team-info">
-            <h3>Reza Pradana</h3>
-            <p>Universitas Pamulang</p>
-            <p>Front-End Developer</p>
-        </div>
-    </div>
-
-        <div class="team-member" data-aos="fade-up" data-aos-duration="1200">
-            <img src="path/to/photo3.jpg" alt="Badi Amnu Pramuditnya" class="team-photo">
-        <div class="team-info">
-            <h3>Badi Amnu Pramuditnya</h3>
-            <p>Universitas Pamulang</p>
-            <p>UI/UX Designer</p>
-        </div>
-    </div>
-    </div>
-    `;
-
-// eslint-disable-next-line import/prefer-default-export
 export { createAboutUsTemplate };
